d94fbc26ddaa08235ecd3b205724c9b225fe318b
Jiannei <longjian.huang@foxmail.com>
2021-09-20 10:13:04 +0800

    chore: 纠正依赖版本

--- config/response.php
+++ config/response.php
@@ -9,16 +9,92 @@
  * with this source code in the file LICENSE.
  */
 
-use App\Repositories\Enums\ResponseCodeEnum;
-
 return [
-    'enum' => ResponseCodeEnum::class,
+    /*
+    |--------------------------------------------------------------------------
+    | Set the http status code when the response fails
+    |--------------------------------------------------------------------------
+    |
+    | the reference options are false, 200, 500
+    |
+    | false, stricter http status codes such as 404, 401, 403, 500, etc. will be returned
+    | 200, All failed responses will also return a 200 status code
+    | 500, All failed responses return a 500 status code
+    */
 
-    'validation_error_code' => ResponseCodeEnum::HTTP_UNPROCESSABLE_ENTITY,
+    'error_code' => false,
 
+    // You can use enumerations to define the code when the response is returned,
+    // and set the response message according to the locale
+    //
+    // The following two enumeration packages are good choices
+    //
+    // https://github.com/Jiannei/laravel-enum
+    // https://github.com/BenSampo/laravel-enum
+
+    'enum' => \App\Repositories\Enums\ResponseCodeEnum::class, // \Jiannei\Enum\Laravel\Repositories\Enums\HttpStatusCodeEnum::class
+
+    //  You can set some attributes (eg:code/message/header/options) for the exception, and it will override the default attributes of the exception
+    'exception' => [
+        \Illuminate\Validation\ValidationException::class => [
+            'code' => 422,
+        ],
+        \Illuminate\Auth\AuthenticationException::class => [
+
+        ],
+        \Symfony\Component\HttpKernel\Exception\NotFoundHttpException::class =>[
+            'message' => '',
+        ],
+        \Illuminate\Database\Eloquent\ModelNotFoundException::class => [
+            'message' => '',
+        ],
+    ],
+
+    // Set the structure of the response data
     'format' => [
-        'paginated_resource' => [
-            'data_field' => 'list',
+        'fields' => [
+            'status' => ['alias' => 'status', 'show' => true],
+            'code' => ['alias' => 'code', 'show' => true],
+            'message' => ['alias' => 'message', 'show' => true],
+            'error' => ['alias' => 'error', 'show' => true],
+            'data' => [
+                'alias' => 'data',
+                'show' => true,
+
+                'fields' => [
+                    // When data is nested with data, such as returning paged data, you can also set an alias for the inner data
+                    'data' => ['alias' => 'data', 'show' => true], // data/rows/list
+
+                    'meta' => [
+                        'alia' => 'meta',
+                        'show' => true,
+
+                        'fields' => [
+                            'pagination' => [
+                                'alias' => 'pagination',
+                                'show' => true,
+
+                                'fields' => [
+                                    'total' => ['alias' => 'total', 'show' => true],
+                                    'count' => ['alias' => 'count', 'show' => true],
+                                    'per_page' => ['alias' => 'per_page', 'show' => true],
+                                    'current_page' => ['alias' => 'current_page', 'show' => true],
+                                    'total_pages' => ['alias' => 'total_pages', 'show' => true],
+                                    'links' => [
+                                        'alias' => 'links',
+                                        'show' => true,
+
+                                        'fields' => [
+                                            'previous' => ['alias' => 'previous', 'show' => true],
+                                            'next' => ['alias' => 'next', 'show' => true],
+                                        ],
+                                    ],
+                                ],
+                            ],
+                        ],
+                    ],
+                ],
+            ],
         ],
     ],
 ];
