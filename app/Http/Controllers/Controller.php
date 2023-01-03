57ad34e2704cc1e384f872ec10b83c899b7d8e87
jiannei <longjian.huang@foxmail.com>
2023-01-03 10:30:11 +0800

    chore: laravel-response

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,9 +6,17 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Jiannei\Response\Laravel\Contracts\Format;
 use Jiannei\Response\Laravel\Support\Traits\JsonResponseTrait;
 
 abstract class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests,JsonResponseTrait;
+
+    protected $formatter;
+
+    public function __construct(Format $format)
+    {
+        $this->formatter = $format;
+    }
 }
