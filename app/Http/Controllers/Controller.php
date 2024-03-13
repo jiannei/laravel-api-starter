d9067be614443c11fd2e4d182d4071b1e4a39baa
jiannei <longjian.huang@foxmail.com>
2024-03-13 09:39:38 +0800

    chore: dep upgrade

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -5,8 +5,9 @@ namespace App\Http\Controllers;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
+use Jiannei\Response\Laravel\Support\Traits\JsonResponseTrait;
 
 class Controller extends BaseController
 {
-    use AuthorizesRequests, ValidatesRequests;
+    use AuthorizesRequests, ValidatesRequests, JsonResponseTrait;
 }
